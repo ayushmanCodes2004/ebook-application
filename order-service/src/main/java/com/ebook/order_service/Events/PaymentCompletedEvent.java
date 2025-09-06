f9c224e6b7a7065dd09b6b87a6eab1a3e102ba38
ayushmanCodes2004 <ayushman678@gmail.com>
2025-09-06 13:32:17 +0530

    Events

---
+++ order-service/src/main/java/com/ebook/order_service/Events/PaymentCompletedEvent.java
@@ -0,0 +1,13 @@
+package com.ebook.order_service.Events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentCompletedEvent {
+    private String orderId;
+    private String paymentStatus;
+}
