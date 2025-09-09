d43c0736b5171935045d51b097b275a5594b71e5
ayushmanCodes2004 <ayushman678@gmail.com>
2025-09-09 11:59:39 +0530

    LoggingAspects

---
+++ book-service/src/main/java/com/ebooks/book_service/Aspects/LoggingAspects.java
@@ -0,0 +1,34 @@
+package com.ebooks.book_service.Aspects;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Component
+@Aspect
+@RequiredArgsConstructor
+public class LoggingAspects {
+
+    private final Logger logger = Logger.getLogger(LoggingAspects.class.getName());
+
+    @Around("execution(* com.ebooks.book_service.Service.impl.*.*(..))")
+    public Object logMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().getName();
+        Object[] methodArgs = joinPoint.getArgs();
+
+        logger.info("Entering method: " + methodName + " with arguments: " + Arrays.toString(methodArgs));
+
+        Object result = joinPoint.proceed();
+
+        logger.info("Exiting method: " + methodName + " with result: " + result);
+
+        return result;
+
+    }
+}
---
+++ order-service/src/main/java/com/ebook/order_service/Aspects/LoggingAspects.java
@@ -0,0 +1,34 @@
+package com.ebook.order_service.Aspects;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Component
+@Aspect
+@RequiredArgsConstructor
+public class LoggingAspects {
+
+    private final Logger logger = Logger.getLogger(LoggingAspects.class.getName());
+
+    @Around("execution(* com.ebook.order_service.Service.*.*(..))")
+    public Object logMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().getName();
+        Object[] methodArgs = joinPoint.getArgs();
+
+        logger.info("Entering method: " + methodName + " with arguments: " + Arrays.toString(methodArgs));
+
+        Object result = joinPoint.proceed();
+
+        logger.info("Exiting method: " + methodName + " with result: " + result);
+
+        return result;
+
+    }
+}
