1a8e14e792d14b426d829f4d1e2e3a2a8c605f0b
ayushmanCodes2004 <ayushman678@gmail.com>
2025-09-15 21:33:47 +0530

    UserResponseDTO

---
+++ Auth-Service/src/main/java/com/ebook/Auth_Service/DTO/UserResponseDTO.java
@@ -0,0 +1,10 @@
+package com.ebook.Auth_Service.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+    private String userName;
+    private String customerId;
+    private String email;
+}
